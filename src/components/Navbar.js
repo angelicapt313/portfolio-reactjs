c40adf6b8784445b589f41831656185e894983fd
Ag <puchetatome.angelica@gmail.com>
2024-05-27 17:25:33 -0600

    Modified navigation bar.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,9 +7,7 @@ export const Navbar = () => {
         <nav>
             <ul className="navbar-links">
                 <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/services">Services</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
+                <li><Link to="/projects">Projects</Link></li>
             </ul>
         </nav>
 
