1da9f965e306ff13c943020e5c55b05afeedd1c9
Ag <puchetatome.angelica@gmail.com>
2024-05-29 16:59:34 -0600

    Updated home component with new content.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,19 @@
 import React from "react";
+import '../styles/Home.css';
+import dev from '../assets/dev.svg'
 
- const Home = () => {
-    return(
+const Home = () => {
+    return (
+        <div className="home flex items-center justify-center min-h-screen">
+            <div className="content">
+                <h1 className="text-4xl text-center my-3 px-1">Hi! My name is <span className="developer-name">Angelica Pucheta</span></h1>
 
-        <div>
-            <h1>Home Page</h1>
+                <p className="my-3 text-center text-xl">I'm front end developer.</p>
+
+                <img alt='dev' className="dev" src={dev} />
+            </div>
         </div>
-    )
+    );
 }
 
 export default Home;
